e22b3d848ddaa0a08320bc153e685dbf86b3db3d
Johannes Neumeier <hello@johannesneumeier.com>
2020-09-07 18:35:25 +0300

    Updated designspace script to new ESHP axis mappings

--- production/make_designspace.py
+++ production/make_designspace.py
@@ -4,7 +4,7 @@ A helper script to create a designspace for Handjet
 ds_path = "production/Handjet.designspace"
 
 # The default instance
-default = (400, 900, 1.0)
+default = (400, 8.00, 1.0)
 
 # Values for the three axis (value, is master, instance name):
 # - Each entry is a design brace layer in the glyphs file
@@ -27,49 +27,46 @@ wghts = [
 
 # The shapes from the ESHP-xxxx glyph brace layer (In comments original
 # "design" names)
+# NOTE: In the design sources these brace layer value are multiplied by 100 to
+# work around the limitation of not having decimal dots in the layer name.
 ESHPs = [
-    (100, True, "Empty"),
-    (200, True, "Triangle"),
-    (300, True, "Square"),
-    (333, True, None),
-    (349, False, "Lozerge"),
-    (366, True, None),
-    (400, True, None),
-    (425, False, None),
-    (450, False, "Rectangle"),
-    (500, True, "Bar (Vertical)"),
-    (533, True, None),  # Tilted rectangle 30
-    (550, False, "Bar (Diagonal Up)"),
-    (566, True, None),  # Tilted rectangle 60
-    (600, True, "Bar (Horizontal)"),
-    (633, True, None),  # Tilted rectangle 120
-    (650, False, "Bar (Diagonal Down)"),
-    (666, True, None),  # Tilted rectangle 150
-    (700, True, None),  # Tilted rectangle 180
-    (800, True, None),  # Square transition
-    (820, False, "Rounded square"),
-    (875, False, "Squircle"),
-    (900, True, "Circle"),
-    (933, False, "Egg"),
-    (950, False, "Oval"),
-    (1000, True, None),  # Oval
-    (1033, True, None),  # Tilted oval 30
-    (1066, True, None),  # Tilted oval 60
-    (1100, True, None),  # Tilted oval 90
-    (1133, True, None),  # Tilted oval 120
-    (1166, True, None),  # Tilted oval 150
-    (1200, True, None),  # Tilted oval 180
-    (1300, True, "Circle transition"),
-    (1350, False, "Clover"),
-    (1400, True, "Flower"),
-    (1500, True, "Star (Diagonal)"),
-    (1533, True, None),  # Tilted star 30
-    (1566, True, None),  # Tilted star 60
-    (1600, True, None),  # Tilted star 90
-    (1650, False, None),  # Big Star
-    (1700, True, "Spindle"),
-    (1737, False, None),  # Pin
-    (1800, True, "Heart"),
+    (0.00, True, "Blank"),
+    (1.00, True, "Triangle"),
+    (2.00, True, "Square"),
+    (2.11, True, None),
+    (2.25, True, "Lozerge"),
+    (2.36, True, None),
+    (2.50, True, None),
+    (3.19, False, "Block"),
+    (3.36, False, "Rectangle"),
+    (4.00, True, "Bar (Vertical)"),
+    (4.11, True, None),
+    (4.25, True, "Bar (Diagonal Up)"),
+    (4.36, True, None),
+    (4.50, True, "Bar (Horizontal)"),
+    (4.61, True, None),
+    (4.75, True, "Bar (Diagonal Down)"),
+    (4.86, True, None),
+    (5.00, True, None),
+    (6.50, True, None),
+    (6.80, False, "Rounded square"),
+    (7.63, False, "Squircle"),
+    (8.00, True, "Circle"),
+    (8.69, False, "Egg"),
+    (8.86, False, "Oval"),
+    (9.50, True, "Thin Oval"),
+    (11.00, True, None),
+    (12.00, False, "Clover"),
+    (13.00, True, "Flower"),
+    (14.00, True, "Star"),
+    (14.11, True, None),
+    (14.25, True, "Star (Diagonal)"),
+    (14.36, True, None),
+    (14.50, True, None),
+    (14.75, False, "Big Star"),
+    (15.00, True, "Spindle"),
+    (15.37, False, "Pin"),
+    (16.00, True, "Heart")
 ]
 
 # The grid configurations from the EGRD-xxxx glyph brace layer
@@ -93,7 +90,7 @@ code = """<?xml version='1.0' encoding='UTF-8'?>
     <axis tag="wght" name="Weight"
         minimum="100" maximum="900" default="400" />
     <axis tag="ESHP" name="Element Shape"
-        minimum="100" maximum="1800" default="900" />
+        minimum="0.00" maximum="16.00" default="8.00" />
     <axis tag="EGRD" name="Element Grid"
         minimum="1.0" maximum="3.0" default="1.0" />
 </axes>
@@ -111,42 +108,40 @@ code = """<?xml version='1.0' encoding='UTF-8'?>
       <key>public.skipExportGlyphs</key>
       <array>
         <string>.notef</string>
+        <string>pixel.ESHP-0</string>
         <string>pixel.ESHP-100</string>
-        <string>pixel.ESHP-1000</string>
-        <string>pixel.ESHP-1033</string>
-        <string>pixel.ESHP-1066</string>
+        <string>pixel.ESHP-200</string>
+        <string>pixel.ESHP-211</string>
+        <string>pixel.ESHP-225</string>
+        <string>pixel.ESHP-236</string>
+        <string>pixel.ESHP-250</string>
+        <string>pixel.ESHP-400</string>
+        <string>pixel.ESHP-411</string>
+        <string>pixel.ESHP-425</string>
+        <string>pixel.ESHP-436</string>
+        <string>pixel.ESHP-450</string>
+        <string>pixel.ESHP-461</string>
+        <string>pixel.ESHP-475</string>
+        <string>pixel.ESHP-486</string>
+        <string>pixel.ESHP-500</string>
+        <string>pixel.ESHP-650</string>
+        <string>pixel.ESHP-800</string>
+        <string>pixel.ESHP-950</string>
         <string>pixel.ESHP-1100</string>
-        <string>pixel.ESHP-1133</string>
-        <string>pixel.ESHP-1166</string>
-        <string>pixel.ESHP-1200</string>
         <string>pixel.ESHP-1300</string>
         <string>pixel.ESHP-1400</string>
+        <string>pixel.ESHP-1411</string>
+        <string>pixel.ESHP-1425</string>
+        <string>pixel.ESHP-1436</string>
+        <string>pixel.ESHP-1450</string>
         <string>pixel.ESHP-1500</string>
-        <string>pixel.ESHP-1533</string>
-        <string>pixel.ESHP-1566</string>
         <string>pixel.ESHP-1600</string>
-        <string>pixel.ESHP-1700</string>
-        <string>pixel.ESHP-1800</string>
-        <string>pixel.ESHP-200</string>
-        <string>pixel.ESHP-300</string>
-        <string>pixel.ESHP-333</string>
-        <string>pixel.ESHP-366</string>
-        <string>pixel.ESHP-400</string>
-        <string>pixel.ESHP-500</string>
-        <string>pixel.ESHP-533</string>
-        <string>pixel.ESHP-566</string>
-        <string>pixel.ESHP-600</string>
-        <string>pixel.ESHP-633</string>
-        <string>pixel.ESHP-666</string>
-        <string>pixel.ESHP-700</string>
-        <string>pixel.ESHP-800</string>
-        <string>pixel.ESHP-900</string>
-        <string>pixel.EGRD-10</string>
-        <string>pixel.EGRD-100</string>
-        <string>pixel.EGRD-50</string>
         <string>pixel.wght-100</string>
         <string>pixel.wght-400</string>
         <string>pixel.wght-900</string>
+        <string>pixel.EGRD-1</string>
+        <string>pixel.EGRD-2</string>
+        <string>pixel.EGRD-3</string>
       </array>
     </dict>
   </lib>
@@ -225,14 +220,14 @@ def write_designspace():
                         # decimal to comply with the initial EGRD axis
                         # defintion which features decimals (to hint for apps
                         # what precision to use in the UI for this axis)
-                        brace = "{%d,%d,%d}" % (w, s, int(g))
+                        brace = "{%d,%d,%d}" % (w, s * 100, int(g))
                         sources += source % (brace, brace,
                                              w, s, "{0:0.1f}".format(g))
 
                 # If shape and grid are the default, then write a named fvar
                 # <instance> for this combination (weight)
                 if is_shape_default and is_grid_default:
-                    brace = "{%d,%d,%d}" % (w, s, g)
+                    brace = "{%d,%d,%d}" % (w, s * 100, g)
                     stylename = weight_name
                     instances += instance % (brace, brace, stylename,
                                              w, s, "{0:0.1f}".format(g))
